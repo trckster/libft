3124da5f07e8a6978dd7e83f05d81821426b9916
Daniil <dan-trofimov@mail.ru>
2019-09-18 01:28:35 +0300

    next one!

---
+++ ft_strncat.c
@@ -0,0 +1,17 @@
+#include "libft.h"
+
+char	*ft_strncat(char *dest, const char *src, size_t n)
+{
+	int	dest_len;
+	size_t	i;
+
+	dest_len = ft_strlen(dest);
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = 0;
+	return (dest);
+}
