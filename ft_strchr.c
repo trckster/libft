25738a6652fdcb75b7a0a167a056123eaecaa018
Daniil <dan-trofimov@mail.ru>
2019-09-18 02:11:46 +0300

    next one!

---
+++ ft_strchr.c
@@ -0,0 +1,19 @@
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+	char	*t;
+
+	t = (char *) s;
+	i = 0;
+	while (t[i])
+	{
+		if ((char)c == t[i])
+			return (t + i);
+		i++;
+	}
+	if ((char) c == t[i])
+		return (t + i);
+	return (0);
+}
