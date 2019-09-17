be3703bf6c29c248a6d6afc857ce3e18409ca1dd
Bertha Kayleen <bkayleen@su-e2.21-school.ru>
2019-09-17 22:27:21 +0300

    new day

---
+++ ft_memmove.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkayleen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/17 22:00:09 by bkayleen          #+#    #+#             */
+/*   Updated: 2019/09/17 22:26:57 by bkayleen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char		*d;
+	const unsigned char	*s;
+	size_t				i;
+
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	if (d < s)
+	{
+		i = -1;
+		while (++i < len)
+			d[i] = s[i];
+	}
+	else
+	{
+		i = len - 1;
+		while (1)
+		{
+			d[i] = s[i];
+			if (!i)
+				break ;
+			i--;
+		}
+	}
+	return (dst);
+}
