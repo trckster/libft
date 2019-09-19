5dbd7c70f10801df33990e37dc740dd68a2bde6d
Bertha Kayleen <bkayleen@su-e2.21-school.ru>
2019-09-19 21:45:39 +0300

    new

---
+++ ft_strsub.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkayleen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/19 21:34:24 by bkayleen          #+#    #+#             */
+/*   Updated: 2019/09/19 21:44:59 by bkayleen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*res;
+
+	if (!(res = (char *)malloc(len + 1)))
+		return (0);
+	i = 0;
+	while (i < len)
+	{
+		res[i] = s[start + i];
+		i++;
+	}
+	res[i] = 0;
+	return (res);
+}
